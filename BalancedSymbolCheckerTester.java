e42fc66dd4c2d34fa10887b811e909cef052ccf9
mikeyeyer098 <47938453+mikeyeyer098@users.noreply.github.com>
2020-02-24 14:55:04 -0700

    Add files via upload

--- BalancedSymbolCheckerTester.java
+++ BalancedSymbolCheckerTester.java
@@ -3,19 +3,19 @@ package assign06;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 
 import org.junit.jupiter.api.Test;
 
 class BalancedSymbolCheckerTester {
 
 	@Test
-	void test1() {
+	void test1() throws FileNotFoundException {
 		
-		String filename = "src/assign06/try.txt";
+		String filename = "/Users/michaeleyer/Mirror/School/CS 2420/CS2420Workspace/Assignments/src/assign06/try.txt";
 		File x = new File(filename);
 		System.out.println(x.exists()); // this prints true
 		BalancedSymbolChecker.checkFile(filename); // so why does this have an error?
-
 	}
 	
 
